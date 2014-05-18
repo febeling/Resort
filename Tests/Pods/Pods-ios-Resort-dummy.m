1bb9bc467126b902f5958e1fb6a3aa0205071b8e
Florian Ebeling <florian.ebeling@gmail.com>
2014-05-18 15:45:32 +0200

    Add the pods

---
+++ Tests/Pods/Pods-ios-Resort-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ios_Resort : NSObject
+@end
+@implementation PodsDummy_Pods_ios_Resort
+@end
---
+++ Tests/Pods/Pods-ios-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Expecta
+#define COCOAPODS_POD_AVAILABLE_Expecta
+#define COCOAPODS_VERSION_MAJOR_Expecta 0
+#define COCOAPODS_VERSION_MINOR_Expecta 2
+#define COCOAPODS_VERSION_PATCH_Expecta 4
+
+// OCMock
+#define COCOAPODS_POD_AVAILABLE_OCMock
+#define COCOAPODS_VERSION_MAJOR_OCMock 2
+#define COCOAPODS_VERSION_MINOR_OCMock 1
+#define COCOAPODS_VERSION_PATCH_OCMock 1
+
+// Resort
+#define COCOAPODS_POD_AVAILABLE_Resort
+#define COCOAPODS_VERSION_MAJOR_Resort 0
+#define COCOAPODS_VERSION_MINOR_Resort 1
+#define COCOAPODS_VERSION_PATCH_Resort 0
+
